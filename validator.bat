c4b1ddc8b0230931638df19b06622e61d70420ed
Berkkenz <155427440+Berkkenz@users.noreply.github.com>
2024-09-28 19:20:14 -0400

    Update validator.bat

--- validator.bat
+++ validator.bat
@@ -1,13 +1,21 @@
 @echo off
-REM ELEVATION
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    echo This script requires administrator privileges.
-    echo Please approve the elevation prompt.
-	 powershell -Command "Start-Process '%~f0' -Verb runAs"
-    exit /b
+REM GIT VALIDATION
+if exist "%TEMP%\gitinstaller.exe" (
+	git --version 2>nul
+	if errorlevel 1 (
+		where git 2>nul
+		if errorlevel 1 (
+			del /f /q "%TEMP%\gitinstaller.exe"
+			exit /b 1
+		) else (
+			del /f /q "%TEMP%\gitinstaller.exe"
+			exit /b 0
+		)
+	) else (
+		del /f /q "%TEMP%\gitinstaller.exe"
+		exit /b 0
+	)
 )
-
 REM JAVA 8 VALIDATION
 if exist "%TEMP%\java8installer.exe" (
 	java -version 2>nul
