c52150224e7ee79b6c84d4813029925566e95cb9
Berkkenz <155427440+Berkkenz@users.noreply.github.com>
2024-09-29 06:37:51 -0400

    Update forge.bat

---
+++ forge.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "MC=%APPDATA%\.minecraft"
+set "LR=%APPDATA%\BerkkenzMC"
+
+if not exist "%MC%\versions\1.20.1-forge-47.3.10" (
+	cls
+	echo Forge not installed. Attempting download...
+	curl -L -o "%TEMP%\forgeinstaller.jar" "https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.3.10/forge-1.20.1-47.3.10-installer.jar"
+	if errorlevel 1 (
+		cls
+		echo Forge download has failed. Exiting...
+		del /s /f "%TEMP%\forgeinstaller.jar"
+		pause
+		exit /b 1
+	)
+	cls
+	echo Forge download successful. Attempting install...
+	if not exist "%MC%\versions\1.20.1" (
+		xcopy "%LR%\1.20.1" "%MC%\versions\1.20.1" /e /i /h /y
+	)
+	%JAVA8% -jar "%TEMP%\forgeinstaller.jar" --installClient
+	if errorlevel 1 (
+		cls
+		echo Forge install has failed. Exiting...
+		del /s /f "%TEMP%\forgeinstaller.jar"
+		pause
+		exit /b 1
+	) else (
+		cls
+		echo Forge has been installed successfully. Proceeding...
+	)
+) else (
+	cls
+	echo Correct Forge is already installed. Proceeding...
+)
